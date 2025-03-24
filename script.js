464fbdacce33ec7987c4e11dd8749c98c6a018d8
djk801 <dongjoonkwak@gmail.com>
2025-03-24 23:45:05 +0900

    first add

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("data.json")
+      .then(response => response.json())
+      .then(quotes => {
+          let randomIndex = Math.floor(Math.random() * quotes.length);
+          let quoteData = quotes[randomIndex];
+
+          document.getElementById("quote-text").textContent = quoteData.quote;
+          document.getElementById("quote-tags").textContent = quoteData.tags[0];
+      })
+      .catch(error => console.error("Error loading quotes:", error));
+});
+
+
+document.querySelector(".container").addEventListener("click", function () {
+  document.querySelector(".card").classList.toggle("flipped");
+  setTimeout(() => {
+    document.getElementById("refreshButton").style.opacity='1';
+  }, 500);
+});
